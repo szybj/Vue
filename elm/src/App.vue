00d26d6c18693d6baa241e57160b7b73483122b4
szybj <songzhiyin@outlook.com>
2017-02-09 22:15:57 +0800

    elm

---
+++ elm/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">tab</div>
+    <div class="content">content</div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/header/header.vue';
+  export default {
+    components: {
+     'v-header': Header
+    }
+  };
+</script>
+
+<style>
+
+</style>
